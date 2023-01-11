96f6ecd5fd78ebf953593cf8e90b3f551ef21f65
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2023-01-10 22:09:58 -0800

    Windows: use consistent ldflags on 32/64 bit
    Use /force:multiple everywhere
    
    Propagate WINDOWS_SUBSYSTEM on amd64.

--- platform/windows.mk
+++ platform/windows.mk
@@ -16,17 +16,19 @@ CFLAGS+=\
 CFLAGS+=-static-crt
 LDFLAGS+=-static-crt
 
+WINDOWS_SUBSYSTEM?=windows
+
 ifdef WIN64
 CFLAGS += -mamd64
 LDFLAGS += -mamd64
+LDFLAGS += /link /subsystem:$(WINDOWS_SUBSYSTEM) /force:multiple
 else
 CFLAGS += -DXP_SUPPORT
 XP_SUPPORT:=y
 XP_SUPPORT_OBJS := \
    $(MAKEFILES_ROOT)src/xpsup-c.o \
    $(MAKEFILES_ROOT)src/xpsup.o
-WINDOWS_SUBSYSTEM?=windows
-LDFLAGS += $(XP_SUPPORT_OBJS) /link /subsystem:$(WINDOWS_SUBSYSTEM),5.01 /force
+LDFLAGS += $(XP_SUPPORT_OBJS) /link /subsystem:$(WINDOWS_SUBSYSTEM),5.01 /force:multiple
 
 $(MAKEFILES_ROOT)src/xpsup-c.o: $(MAKEFILES_ROOT)src/xpsup-c.c
 $(MAKEFILES_ROOT)src/xpsup.o: $(MAKEFILES_ROOT)src/xpsup.asm
