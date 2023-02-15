4355676ddc290c8b110740a8fada5ad643247c72
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2023-02-15 05:49:38 +0000

    Previous commit should be clang only

--- platform/unix.mk
+++ platform/unix.mk
@@ -5,9 +5,11 @@
 CC:=$(shell $(MAKEFILES_ROOT)scripts/find-cc.sh cc clang gcc)
 CXX:=$(shell $(MAKEFILES_ROOT)scripts/find-cc.sh c++ clang++ g++)
 
+ifeq (1, $(shell echo $(CC) | grep -c clang))
 CFLAGS+=-Wno-unknown-warning-option
 CFLAGS+=-Wno-gnu-zero-variadic-macro-arguments
 CXXFLAGS+=-Wno-c99-extensions
+endif
 
 CXXFLAGS+=-std=c++11
 
