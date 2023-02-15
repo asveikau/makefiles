298887ef90a7ffd417cd3ff9393b5155bfe25d1b
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2023-02-14 21:35:23 -0800

    Add a few warning flags found when building a code base with recent clang
    * Don't warn for extra comma in __VA_ARGS__ expansion
        - Apparently this will be valid in C++20.  MSVC, GCC and clang are all ok
          with it.
    * Don't warn for unknown warning options
        - Since I think the above one is kind of new?
    * Don't warn for C99 in C++
        - MSVC may reject some of this, but maybe that's OK.

--- platform/unix.mk
+++ platform/unix.mk
@@ -5,6 +5,10 @@
 CC:=$(shell $(MAKEFILES_ROOT)scripts/find-cc.sh cc clang gcc)
 CXX:=$(shell $(MAKEFILES_ROOT)scripts/find-cc.sh c++ clang++ g++)
 
+CFLAGS+=-Wno-unknown-warning-option
+CFLAGS+=-Wno-gnu-zero-variadic-macro-arguments
+CXXFLAGS+=-Wno-c99-extensions
+
 CXXFLAGS+=-std=c++11
 
 LIBPREFIX:=lib
